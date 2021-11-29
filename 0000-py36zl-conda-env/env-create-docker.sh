59f8b9641956a21e183ec45028b26e1973381ac5
cs224 <cs224@weisser-zwerg.dev>
2021-11-29 13:04:40 +0100

    0000-py36zl-conda-env/Makefile

--- 0000-py36zl-conda-env/env-create-docker.sh
+++ 0000-py36zl-conda-env/env-create-docker.sh
@@ -3,11 +3,6 @@
 # exit when any command fails
 set -e
 
-echo ">> Updating base environemnt"
-conda update -y conda --no-pin
-conda install mamba -n base -c conda-forge
-pip install --upgrade pip
-
 echo ">> Setting-up py36zl environemnt"
 mamba env create -f environment.yml
 
